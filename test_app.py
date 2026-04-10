27823dcf496780838ff4298ff63ff715a841dd73
Hamza El Hilali <hamza.elhilali@dcmail.ca>
2026-04-09 22:38:05 -0400

    Fix encoding + add unit tests by Hamza (closes #2)

---
+++ test_app.py
@@ -0,0 +1,16 @@
+﻿import unittest
+from app import greet, farewell
+
+class TestApp(unittest.TestCase):
+
+    def test_greet(self):
+        result = greet("Hamza")
+        self.assertIn("Hamza", result)
+        self.assertIn("AIDI 2004", result)
+
+    def test_farewell(self):
+        result = farewell("Hamza")
+        self.assertIn("Hamza", result)
+
+if __name__ == "__main__":
+    unittest.main()
